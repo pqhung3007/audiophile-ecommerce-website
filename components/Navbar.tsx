dd98b253bd9edc994ce7b860b54829408bd90dbd
Quang Hung <hungpham307@gmail.com>
2022-12-01 16:32:29 +0700

    fix: button always appear

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,9 +20,9 @@ function Navbar() {
 
   return (
     <>
-      <div className="fixed z-30 flex h-16 w-full items-center justify-between bg-black px-6 lg:px-16">
+      <div className="z-30 flex h-16 w-full items-center justify-between bg-black px-6 lg:px-16">
         {/* Hamburger */}
-        <button onClick={handleOpenMenu}>
+        <button className="block lg:hidden" onClick={handleOpenMenu}>
           <Image
             src={`${
               isOpen
