c43afaa1aea52e11fd4655f387b8070b436d9015
Quang Hung <hungpham307@gmail.com>
2022-12-01 10:50:43 +0700

    extract link items

---
+++ utils/links.ts
@@ -0,0 +1,20 @@
+export const links = [
+  {
+    id: 1,
+    text: "headphones",
+    url: "/headphones",
+    imageLink: "/assets/shared/desktop/image-category-thumbnail-headphones.png",
+  },
+  {
+    id: 2,
+    text: "speakers",
+    url: "/speakers",
+    imageLink: "/assets/shared/desktop/image-category-thumbnail-speakers.png",
+  },
+  {
+    id: 3,
+    text: "earphones",
+    url: "/earphones",
+    imageLink: "/assets/shared/desktop/image-category-thumbnail-earphones.png",
+  },
+];
