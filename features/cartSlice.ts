1056c21ba9fdccfe656e8cf4d359952913f4c373
Quang Hung <hungpham307@gmail.com>
2022-12-12 11:21:54 +0700

    feat: add redux toolkit

---
+++ features/cartSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: initialState,
+  reducers: {},
+});
+
+export default cartSlice.reducer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../features/store";
 import Navbar from "../components/shared/Navbar";
 import Footer from "../components/shared/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </Provider>
   );
 }
