2b5df9cfc48a3d906e1ef6adf2ca3d643a7924f6
Quang Hung <hungpham307@gmail.com>
2022-12-13 14:10:25 +0700

    handle cart empty state

--- components/CartDialog.tsx
+++ components/CartDialog.tsx
@@ -11,7 +11,6 @@ import {
   totalPrice,
 } from "../features/cartSlice";
 import ProductQuantity from "./ProductQuantity";
-import ForwardButton from "./ForwardButton";
 
 interface DialogProps {
   isModalOpen: boolean;
@@ -39,53 +38,72 @@ export default function CartDialog({
       {/* Full-screen container to center the panel */}
       <div className="fixed inset-0 flex items-center justify-center p-4">
         {/* The actual dialog panel  */}
-        <Dialog.Panel className="mx-auto max-w-sm space-y-6 rounded bg-white p-6">
-          <div className="mb-4 flex justify-between">
-            <h3 className="text-lg font-bold uppercase">cart ({quantity})</h3>
-            <button
-              className="text-neutral-500 hover:underline"
-              onClick={() => dispatch(clearCart())}
-            >
-              Remove all
-            </button>
-          </div>
-
-          <div className="space-y-6">
-            {items.map((item, index) => (
-              <div
-                className="flex items-center justify-between gap-4"
-                key={index}
-              >
-                <div className="h-16 w-16 object-cover">
-                  <Image
-                    src={"/" + item.image}
-                    width={64}
-                    height={64}
-                    alt=""
-                    className="rounded-lg"
-                  />
-                </div>
-                <div>
-                  <p className="font-bold">{item.name}</p>
-                  <p className="font-bold text-neutral-500">$ {item.price}</p>
-                </div>
-                <ProductQuantity
-                  quantity={item.quantity}
-                  increment={() => dispatch(increaseQuantity(item.id))}
-                  decrement={() => dispatch(decreaseQuantity(item.id))}
-                />
+        <Dialog.Panel className="mx-auto w-full max-w-sm space-y-6 rounded bg-white p-6">
+          {items.length > 0 ? (
+            <div>
+              <div className="mb-4 flex justify-between">
+                <h3 className="text-lg font-bold uppercase">
+                  cart ({quantity})
+                </h3>
+                <button
+                  className="text-neutral-500 hover:underline"
+                  onClick={() => dispatch(clearCart())}
+                >
+                  Remove all
+                </button>
               </div>
-            ))}
-          </div>
 
-          <div className="mt-8 flex justify-between">
-            <p className="uppercase text-neutral-500">total</p>
-            <div className="text-lg font-bold">$ {price}</div>
-          </div>
+              <div className="space-y-6">
+                {items.map((item, index) => (
+                  <div
+                    className="flex items-center justify-between gap-4"
+                    key={index}
+                  >
+                    <div className="h-16 w-16 object-cover">
+                      <Image
+                        src={"/" + item.image}
+                        width={64}
+                        height={64}
+                        alt=""
+                        className="rounded-lg"
+                      />
+                    </div>
+                    <div>
+                      <p className="font-bold">{item.name}</p>
+                      <p className="font-bold text-neutral-500">
+                        $ {item.price}
+                      </p>
+                    </div>
+                    <ProductQuantity
+                      quantity={item.quantity}
+                      increment={() => dispatch(increaseQuantity(item.id))}
+                      decrement={() => dispatch(decreaseQuantity(item.id))}
+                    />
+                  </div>
+                ))}
+              </div>
 
-          <button className="w-full border-none bg-accent px-6 py-3 text-sm font-semibold uppercase tracking-wider text-white duration-200 hover:bg-accent-hover">
-            checkout
-          </button>
+              <div className="mt-8 flex justify-between">
+                <p className="uppercase text-neutral-500">total</p>
+                <strong className="text-lg">$ {price}</strong>
+              </div>
+
+              <button className="w-full border-none bg-accent px-6 py-3 text-sm font-semibold uppercase tracking-wider text-white duration-200 hover:bg-accent-hover">
+                checkout
+              </button>
+            </div>
+          ) : (
+            <div className="text-center">
+              <strong>Your cart is empty</strong>
+              <Image
+                src="/assets/cart/cart.svg"
+                width={96}
+                height={96}
+                alt=""
+                className="mx-auto h-32 w-32"
+              />
+            </div>
+          )}
         </Dialog.Panel>
       </div>
     </Dialog>
